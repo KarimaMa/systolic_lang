c33e9f5468b4e312ce8ed9ebb60296661c443361
Karima Ma <karima_ma@berkeley.edu>
2020-05-27 18:32:39 -0400

    added tests

--- grammar.py
+++ grammar.py
@@ -1,6 +1,8 @@
+import numpy as np
+
 class IndexConst:
     def __init__(self, name):
-      self.values = [name[
+      self.values = [name]
 
     def visit(self, env_map):
       e = self.values[0] #char name
@@ -35,12 +37,12 @@ class Access:
     # array of index expressions, which are either IndexConst or Add
     # assuming everything is 2D, 3D, or 4D
     index_exprs = self.values[0] 
-    assert(len(index_exprs) == t.ndims)
-    if t.ndims == 2:
+    assert(len(index_exprs) == t.ndim)
+    if t.ndim == 2:
       return t[index_exprs[0].visit(env_map), index_exprs[1].visit(env_map)]
-    if t.ndims == 3:
+    if t.ndim == 3:
       return t[index_exprs[0].visit(env_map), index_exprs[1].visit(env_map), index_exprs[2].visit(env_map)]
-    if t.ndims == 4:
+    if t.ndim == 4:
       return t[index_exprs[0].visit(env_map), index_exprs[1].visit(env_map), index_exprs[2].visit(env_map), index_exprs[3].visit(env_map)]
 
 
@@ -59,52 +61,87 @@ class SumR:
 
   def visit(self, env_map):
     r = 0.0
-    r_var = self.values[0] 
+    r_var_name = self.values[0].values[0]
     r_dom = self.values[1]
     e = self.values[2]
 
     for val in range(r_dom[0], r_dom[1]):
-      env_map[r_var] = val
+      env_map[r_var_name] = val
       r += e.visit(env_map)
 
     return r
 
 
 class Gen:
-  def __init__(self, dim_sizes, lh_index_exprs, rhs):
-    self.values = [dim_sizes, lh_index_exprs, rhs]
+  def __init__(self, dim_sizes, lhs_index_exprs, rhs):
+    self.values = [dim_sizes, lhs_index_exprs, rhs]
 
-  def subGen(self, lhs, lh_index_exprs, dim_id, dim_sizes, rhs, env_map):
+  def subGen(self, lhs, lhs_index_exprs, dim_id, dim_sizes, rhs, env_map):
     dim_size = dim_sizes[dim_id]
     for val in range(dim_size):
-      dim_name = lh_index_exprs[dim_id].values[0]
+      dim_name = lhs_index_exprs[dim_id].values[0]
       env_map[dim_name] = val
 
-      if dim_id == (len(lh_index_exprs) - 1): # all indices have been determined
-        if len(lh_index_exprs) == 2:
+      if dim_id == (len(lhs_index_exprs) - 1): # all indices have been determined
+        if len(lhs_index_exprs) == 2:
           lhs[lhs_index_exprs[0].visit(env_map), lhs_index_exprs[1].visit(env_map)] = rhs.visit(env_map)
-        if len(lh_index_exprs) == 3:
+        if len(lhs_index_exprs) == 3:
           lhs[lhs_index_exprs[0].visit(env_map), lhs_index_exprs[1].visit(env_map), lhs_index_exprs[2].visit(env_map)] = rhs.visit(env_map)
-        if len(lh_index_exprs) == 4:
+        if len(lhs_index_exprs) == 4:
           lhs[lhs_index_exprs[0].visit(env_map), lhs_index_exprs[1].visit(env_map), lhs_index_exprs[2].visit(env_map), lhs_index_exprs[3].visit(env_map)] = rhs.visit(env_map)
 
       else:
-        self.subGen(lhs, lh_index_exprs, dim_id+1, dim_sizes, rhs, env_map)
+        self.subGen(lhs, lhs_index_exprs, dim_id+1, dim_sizes, rhs, env_map)
 
 
   def visit(self, env_map):
     # array of dimension sizes
     dim_sizes = self.values[0]
     # array of dimension names, should be type index const for now
-    lh_index_exprs = self.values[1]
+    lhs_index_exprs = self.values[1]
     rhs = self.values[2]
 
     t = np.zeros(dim_sizes)
-    self.subGen(t, lh_index_exprs, 0, dim_sizes, rhs, env_map)
+    self.subGen(t, lhs_index_exprs, 0, dim_sizes, rhs, env_map)
     return t
 
+rht = np.random.randint(0,10,(3,3,2))
+print(rht)
+t = Tensor(rht)
+rh_indices = [IndexConst('x'), IndexConst('y'), IndexConst('z')]
+access = Access(rh_indices, t)
+lht_sizes = [2,3,2]
+
+lh_indices = [IndexConst('x'), IndexConst('y'), IndexConst('z')]
+gen = Gen(lht_sizes, lh_indices, access)
+result = gen.visit({})
+
+print(result)
+print("---------- Testing MATMUL ------------")
+
+A = np.random.randint(0,5,(2,2))
+B = np.random.randint(0,5,(2,2))
+print("A")
+print(A)
+print("B")
+print(B)
+
+At = Tensor(A)
+Bt = Tensor(B)
+Aindices = [IndexConst('i'), IndexConst('k')]
+Bindices = [IndexConst('k'), IndexConst('j')]
+Aaccess = Access(Aindices, At)
+Baccess = Access(Bindices, Bt)
+matprod = Product(Aaccess, Baccess)
+matsum = SumR(IndexConst('k'), (0,2), matprod)
+
+lht_sizes = [2,2]
+lh_indices = [IndexConst('i'), IndexConst('j')]
+gen = Gen(lht_sizes, lh_indices, matsum)
+result = gen.visit({})
+print("C")
+print(result)
 
 
-    
-    
-    
+print("---------- Testing CONV ------------")
+
